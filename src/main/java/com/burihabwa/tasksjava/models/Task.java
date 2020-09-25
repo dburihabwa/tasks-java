201ff2fc18bec471aff7c81474a1dde7234499f4
Dorian Burihabwa <dorian@burihabwa.com>
2020-09-25 17:48:48 +0200

    Add spring data annotation to Task class

--- src/main/java/com/burihabwa/tasksjava/models/Task.java
+++ src/main/java/com/burihabwa/tasksjava/models/Task.java
@@ -1,20 +1,36 @@
 package com.burihabwa.tasksjava.models;
 
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDateTime;
 
+@Document
 public class Task {
+    @Id
+    private String id;
     private String title;
     private String description;
     private LocalDateTime start;
     private LocalDateTime finish;
     private boolean completed;
 
-    public Task(String title, String description, LocalDateTime start, LocalDateTime finish, boolean completed) {
+    public Task(String id, String title, String description, LocalDateTime start, LocalDateTime finish, boolean completed) {
+        this.id = id;
         this.title = title;
         this.description = description;
         this.start = start;
         this.finish = finish;
         this.completed = completed;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getTitle() {
