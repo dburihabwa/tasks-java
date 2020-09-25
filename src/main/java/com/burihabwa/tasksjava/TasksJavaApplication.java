ea51f1f37bbdc50e96f45e65158b46a418999180
Dorian Burihabwa <dorian@burihabwa.com>
2020-09-25 14:33:00 +0200

    First commit

---
+++ src/main/java/com/burihabwa/tasksjava/TasksJavaApplication.java
@@ -0,0 +1,13 @@
+package com.burihabwa.tasksjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/burihabwa/tasksjava/TasksJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burihabwa.tasksjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
