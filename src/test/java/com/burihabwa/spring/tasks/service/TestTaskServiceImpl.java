e97219dd4f58d39d9045ae3c270e5e21de40aa19
Dorian Burihabwa <dorian@burihabwa.com>
2020-09-28 19:14:59 +0200

    Add basic unit tests for TaskServiceImpl

--- src/test/java/com/burihabwa/spring/tasks/service/TestTaskServiceImpl.java
+++ src/test/java/com/burihabwa/spring/tasks/service/TestTaskServiceImpl.java
@@ -1,16 +1,78 @@
 package com.burihabwa.spring.tasks.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.burihabwa.spring.tasks.dao.FakeTaskDAO;
+import com.burihabwa.spring.tasks.models.Task;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class TestTaskService {
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
-    @Autowired
-    private TaskService service;
+public class TestTaskServiceImpl {
+    @Test
+    public void testNoTask() {
+        TaskServiceImpl service = new TaskServiceImpl(new FakeTaskDAO());
+        Assert.assertEquals(service.list().size(), 0);
+    }
 
     @Test
-    public void test() {
-        Assertions.assertEquals(service.list().size(), 0);
+    public void testAddTask() {
+        TaskServiceImpl service = new TaskServiceImpl(new FakeTaskDAO());
+        Assert.assertEquals(service.list().size(), 0);
+        Task task = new Task(null, "title", "description", LocalDateTime.now(), LocalDateTime.now(), false);
+        Task newTask = service.add(task);
+        Assert.assertNotNull(newTask.getId());
+        Assert.assertEquals(service.list().size(), 1);
+    }
+
+    @Test
+    public void testGetExistingTask() {
+        TaskServiceImpl service = new TaskServiceImpl(new FakeTaskDAO());
+        Task task = new Task(null, "existingTask", "description", LocalDateTime.now(), LocalDateTime.now(), false);
+        Task newTask = service.add(task);
+        Assert.assertEquals(newTask, service.get(newTask.getId()));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testGetNonExistingTask() {
+        TaskServiceImpl service = new TaskServiceImpl(new FakeTaskDAO());
+        service.get(UUID.randomUUID());
+    }
+
+    @Test
+    public void testDeleteExistingTask() {
+        TaskServiceImpl service = new TaskServiceImpl(new FakeTaskDAO());
+        Task task = new Task(null, "existingTask", "description", LocalDateTime.now(), LocalDateTime.now(), false);
+        Task newTask = service.add(task);
+        Assert.assertEquals(service.list().size(), 1);
+        Task deletedTask = service.delete(newTask.getId());
+        Assert.assertEquals(deletedTask, newTask);
+        Assert.assertEquals(service.list().size(), 0);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testDeleteNonExistingTask() {
+        TaskServiceImpl service = new TaskServiceImpl(new FakeTaskDAO());
+        service.delete(UUID.randomUUID());
+    }
+
+    @Test
+    public void testUpdateExistingTask() {
+        TaskServiceImpl service = new TaskServiceImpl(new FakeTaskDAO());
+        Task task = new Task(null, "existingTask", "description", LocalDateTime.now(), LocalDateTime.now(), false);
+        Task newTask = service.add(task);
+        String newTitle = "newTitle";
+        task.setTitle(newTitle);
+        service.update(newTask.getId(), newTask);
+        Task modifiedTask = service.get(newTask.getId());
+        Assert.assertEquals(newTitle, modifiedTask.getTitle());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testUpdateNonExistingTask() {
+        TaskServiceImpl service = new TaskServiceImpl(new FakeTaskDAO());
+        Task task = new Task(null, "existingTask", "description", LocalDateTime.now(), LocalDateTime.now(), false);
+        service.update(UUID.randomUUID(), task);
     }
 }
