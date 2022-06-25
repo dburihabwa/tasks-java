34e75f26a3287463cec3e5054823d47ef3b49875
Dorian Burihabwa <dorian@burihabwa.com>
2022-06-26 00:19:15 +0200

    Comment out test

--- src/test/java/com/burihabwa/spring/tasks/TasksJavaApplicationTests.java
+++ src/test/java/com/burihabwa/spring/tasks/TasksJavaApplicationTests.java
@@ -3,7 +3,7 @@ package com.burihabwa.spring.tasks;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TasksJavaApplicationTests {
 
 	@Test
